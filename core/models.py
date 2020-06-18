d9a8decd578876cf287d7649607b2814eb73194c
Jailtonjps <jailtonps@msn.com>
2020-06-17 23:18:01 -0300

    Incluido alteração do campo datetime

--- core/models.py
+++ core/models.py
@@ -6,8 +6,8 @@ from django.db import models
 class Evento(models.Model):
     titulo = models.CharField(max_length=100)
     descricao = models.TextField(blank=True, null=True)
-    data_evento = models.DateField(verbose_name='Data do Evento')
-    data_criacao = models.DateField(auto_now=True)
+    data_evento = models.DateTimeField(verbose_name='Data do Evento')
+    data_criacao = models.DateTimeField(auto_now=True)
     usuario = models.ForeignKey(User, on_delete=models.CASCADE)
 
 
@@ -18,4 +18,4 @@ class Evento(models.Model):
         return self.titulo
 
     def get_data_evento(self):
-        return self.data_evento.strftime('%d/%m/%Y - %H:%M hrs')
+        return self.data_evento.strftime("%d/%m/%Y %H:%M hrs")
