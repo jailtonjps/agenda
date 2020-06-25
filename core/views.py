89bc1a5a4df7bff29277f624a83e6350650b467e
Jailtonjps <jailtonps@msn.com>
2020-06-25 01:11:54 -0300

    Adicionado retorno de Http404, configurado timezone para pt-br e adicionado para mostrar evendo atrasado da agenda em vermelho

--- core/views.py
+++ core/views.py
@@ -3,6 +3,8 @@ from django.shortcuts import render, redirect
 from core.models import Evento
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login, logout
+from datetime import datetime, timedelta
+from django.http.response import Http404
 
 # Create your views here.
 
@@ -29,7 +31,8 @@ def submit_login(request):
 @login_required(login_url='/login/')
 def lista_eventos(request):
    usuario = request.user
-   evento = Evento.objects.filter(usuario=usuario)
+   data_atual = datetime.now() - timedelta(hours=1)
+   evento = Evento.objects.filter(usuario=usuario, data_evento__gt=data_atual)
    dados = {'eventos':evento}
    return render(request,'agenda.html', dados)
 
@@ -63,7 +66,13 @@ def submit_evento(request):
 @login_required(login_url='/login/')
 def delete_evento(request,id_evento):
     usuario = request.user
-    evento = Evento.objects.get(id=id_evento)
+    try:
+         evento = Evento.objects.get(id=id_evento)
+    except Exception:
+        raise Http404()
+
     if usuario == evento.usuario:
          evento.delete()
+    else:
+        raise  Http404()
     return redirect('/')
